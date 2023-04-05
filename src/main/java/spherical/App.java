ff914b40e57088983b9b5d75f2e597cf281ac5e2
Spherical-S <sadiq.shahid101@gmail.com>
2023-04-05 16:51:21 -0400

    compiled to jar

---
+++ src/main/java/spherical/App.java
@@ -0,0 +1,16 @@
+package spherical;
+
+/*
+@ Author: Sadiq Shahid
+@ Date: March 29th 2023, edited April 2nd 2023
+@ Description: Program utilizes classes to mimick how a banking system would work.
+*/
+
+public class App{
+    public static void main(String[] args) { //main method
+
+        MyFrame frame = new MyFrame();
+
+    }// end of main method
+
+}//end class Main
